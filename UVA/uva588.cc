cef78c6158ccce1673a1e20a0fff385066899607
Guilherme Leobas <guilhermeleobas@gmail.com>
2017-09-20 00:15:30 -0300

    Update uva588.cc
    bellman ford

--- UVA/uva588.cc
+++ UVA/uva588.cc
@@ -1,68 +1,75 @@
-// http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=499
+// https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=499
+// Bellman-ford algorithm
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <set>
+#include <cmath>
+
 
 using namespace std;
 
-typedef vector<vector<pair<int, int> > > graph;
+#define INF 0x3f3f3f3f
+typedef vector<vector<pair<int, int>>> graph;
 
-bool bellman_ford (graph g, int start){
-	
-	vector<int> distance (g.size(), 0x3f3f3f3f);
-	distance[start] = 0;
-
-	for (int i=0; i<g.size(); i++){
-		for (int j=0; j<g.size(); j++){
-			for (int k=0; k<g[j].size(); k++){
-				int w = g[j][k].second;
-				int y = g[j][k].first;
-				int x = j;
-
-				if (distance[y] > distance[x] + w){
-					distance[y] = distance[x] + w;
-				}
-			}
-		}
-	}
-
-	for (int i=0; i<g.size(); i++){
-		for (int j=0; j<g[i].size(); j++){
-			int x = i;
-			int y = g[i][j].first;
-			int w = g[i][j].second;
-			if (distance[x] + w < distance[y]){
-				return false;
-			}
-		}
-	}
-
-	return true;
+bool bellman_ford (const graph& g, int start){
+  vector<int> dist(g.size(), INF);
+  dist[start] = 0;
+  
+  // relax edges
+  for (int i=0; i<g.size(); i++){
+    for (int j=0; j<g.size(); j++){
+      int u = j;
+      for (auto it : g[j]){
+        int v = it.second;
+        int cost = it.first;
+        
+        if (dist[u] + cost < dist[v]){
+          dist[v] = dist[u] + cost;
+        }
+        
+      }
+    }
+  }
+  
+  // check for negative cycle
+  for (int i=0; i<g.size(); i++){
+    int u = i;
+    for (auto it : g[i]){
+      int cost = it.first; 
+      int v = it.second;
+      
+      if (dist[u] + cost < dist[v])
+        return false;
+    }
+  }
+  
+  return true;
 }
 
 int main (){
+  
+  int t;
+  cin >> t;
 
-	int t;
-	cin >> t;
-
-	while (t--){
-		int n, m;
-		cin >> n >> m;
-		
-		graph g (n);
-
-		for (int i=0; i<m; i++){
-			int x, y, w;
-			cin >> x >> y >> w;
-			g[x].push_back (make_pair (y, w));
-		}
-
-		if (bellman_ford (g, 0))
-			cout << "not possible\n";
-		else
-			cout << "possible\n";
-	}
-
-	return 0;
+  while (t--){
+    int n, m;
+    cin >> n >> m;
+    
+    graph g(n);
+    
+    for (int i=0; i<m; i++){
+      int u, v, c;
+      cin >> u >> v >> c;
+      g[u].push_back (make_pair (c, v));
+    }
+    
+    if (bellman_ford (g, 0) == true){
+      cout << "not possible\n";
+    }
+    else{
+      cout << "possible\n";
+    }
+  }
+  return 0;
 }

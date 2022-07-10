2a751e40f072b991886eccb2db944f2d38c4311e
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-07-10 00:32:05 -0300

    Update 55.cc

--- leetcode/55.cc
+++ leetcode/55.cc
@@ -1,18 +1,12 @@
 class Solution {
   public:
     int canJump(std::vector<int>& nums) {
-      std::vector<int> memo (nums.size(), false);
       int valid = nums.size()-1;
-      memo[nums.size()-1] = 1;
       for (int i=nums.size()-2; i >=0; i--) {
         // printf("%d + %d > %d\n", nums[i], i, valid);
-        if (nums[i] + i >= valid) {
+        if (nums[i] + i >= valid)
           valid = i;
-          memo[i] = 1;
-        } else {
-          memo[i] = 0;
-        }
       }
-      return memo[0];
+      return (valid == 0);
     }
 };
